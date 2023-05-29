a1b13548d55eeaf80d2d57e6fec7042b4ffb38b9
Aidar Safiullin <aydarin.tatarin@gmail.com>
2023-05-30 00:47:27 +0300

    Add user's name to navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   
   has_many :notes
+
+  validates :name, presence: true, length: { maximum: 35 }
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "Guest №#{rand(50)}" if self.name.blank?
+  end
 end
