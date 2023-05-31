0b6745e7721792a63737c20fc41a2610eab1397c
Aidar Safiullin <aydarin.tatarin@gmail.com>
2023-06-01 00:22:42 +0300

    Add table to index

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -20,7 +20,7 @@ class NotesController < ApplicationController
 
     respond_to do |format|
       if @note.save
-        format.html { redirect_to note_url(@note), notice: "Note was successfully created." }
+        format.html { redirect_to root_path, notice: "Note was successfully created." }
         format.json { render :show, status: :created, location: @note }
       else
         format.html { render :new, status: :unprocessable_entity }
